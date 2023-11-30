7a97857875b907e0caaad47aca22a5d181918954
Johan Iochem <112541477+JohanIochem@users.noreply.github.com>
2023-11-30 16:44:09 +0100

    Second commit

--- src/main/java/com/jiochem/selenium/exam/exam/ExamApplication.java
+++ src/main/java/com/jiochem/selenium/exam/exam/ExamApplication.java
@@ -1,44 +1,55 @@
 package com.jiochem.selenium.exam.exam;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.interactions.Actions;
 
-@SpringBootApplication
 public class ExamApplication {
 
-		public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 
-			WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 
-			driver.get("https://www.123-stickers.com");
+		driver.get("https://www.123-stickers.com");
 
-			WebElement searchBox = driver.findElement(By.id("search_query_top"));
-			searchBox.sendKeys("toilette");
-			searchBox.submit();
+		WebElement searchBox = driver.findElement(By.id("search_query_top"));
+		searchBox.sendKeys("toilette");
+		searchBox.submit();
 
-			Thread.sleep(2000);
+		Thread.sleep(2000);
 
-			WebElement thirdResult = driver.findElement(By.xpath("(//div[@class='product-container'])[4]"));
-			thirdResult.click();
+		WebElement thirdResult = driver.findElement(By.xpath("(//div[@class='product-container'])[3]"));
+		thirdResult.click();
 
-			WebElement addToCartButton = driver.findElement(By.name("Submit"));
-			addToCartButton.click();
+		Thread.sleep(2000);
 
-			WebElement closePopupButton = driver.findElement(By.xpath("//span[@title='Close window']"));
-			closePopupButton.click();
+		WebElement sizeDropdown = driver.findElement(By.id("attrib-40"));
+		Select sizeSelect = new Select(sizeDropdown);
+		sizeSelect.selectByVisibleText("40");
 
-			WebElement cartIcon = driver.findElement(By.xpath("//b[contains(text(),'Cart')]"));
-			Actions actions = new Actions(driver);
-			actions.moveToElement(cartIcon).perform();
+		WebElement positionToggle = driver.findElement(By.id("attrib-invert"));
+		positionToggle.click();
 
-			WebElement checkoutButton = driver.findElement(By.xpath("//a[@title='Check out']"));
-			checkoutButton.click();
+		WebElement colorDropdown = driver.findElement(By.id("attrib-color"));
+		Select colorSelect = new Select(colorDropdown);
+		colorSelect.selectByVisibleText("Bleu clair");
 
-			driver.quit();
-		}
+		WebElement addToCartButton = driver.findElement(By.name("Submit"));
+		addToCartButton.click();
+
+		WebElement closePopupButton = driver.findElement(By.xpath("//span[@title='Close window']"));
+		closePopupButton.click();
+
+		WebElement cartIcon = driver.findElement(By.xpath("//b[contains(text(),'Cart')]"));
+		Actions actions = new Actions(driver);
+		actions.moveToElement(cartIcon).perform();
+
+		WebElement checkoutButton = driver.findElement(By.xpath("//a[@title='Check out']"));
+		checkoutButton.click();
+
+		driver.quit();
 	}
+}
